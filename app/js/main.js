37ac7618e234313ed6ff1c0e67149a01d4ac9925
andreytutygin <tutygin.a.v@mail.ru>
2021-01-03 18:33:05 +0300

    Installed the swiper.js plugin, made a desktop version of the reviews section.

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
+import Swiper from 'swiper/bundle'
+
 import './components/burger';
 import './components/scroll';
 
+const slider = new Swiper('.reviews-slider', {
+    spaceBetween: 100,
+    speed: 800,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+    },
+});
